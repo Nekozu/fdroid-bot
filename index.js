c2843adc0f65347af8df2a5de09a0f9705b26c95
nekozu <81870902+nekozu@users.noreply.github.com>
2021-10-16 19:55:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-import { Bot } from "grammy";
+const { Bot, InlineKeyboard } = require("grammy");
+const f = require('f-droid')
 
-const bot = new Bot(process.env.token); 
+const bot = new Bot(process.env.TOKEN); 
 
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+bot.command("start", (ctx) =>{
+    const keyboard = new InlineKeyboard().text('Help', 'help')                                                    
+    ctx.reply("Hey! I m bot to search apk at f-droid.org. click help buton for more.", {reply_markup: keyboard })
+})
+
+bot.callbackQuery('help',(ctx)=>{                                                                                                  
+    const keyboard = new InlineKeyboard().text('Back', 'back')                                                                            
+    ctx.api.editMessageText(ctx.chat.id, ctx.msg.message_id, 'How to search apk?\n Just use command /se (apkname). bot just send title url description and license', {reply_markup: keyboard})
+})
+
+bot.callbackQuery("back", (ctx) =>{
+    const keyboard = new InlineKeyboard().text('Help', 'help')                                                                                                              
+    ctx.api.editMessageText(ctx.chat.id, ctx.msg.message_id, "Hey! I m bot to search apk at f-droid.org. click help buton for more.", {reply_markup: keyboard })
+})
+
+bot.command('se',  (ctx) => {          
+          //var pesan = ctx.message.text;
+          let input = ctx.message.text
+           let inputArray = input.split(" ")
+          inputArray.shift()
+          pesan = inputArray.join(" ")
+         f.fsearch(pesan).then(ok => {
+            const res = ok[Math.floor(Math.random() * (ok.length))]
+              const title = res.title
+              const link = res.link
+              const desc = res.desc
+              const license = res.license
+              const key = new InlineKeyboard().url('Apk Link', link)
+          return ctx.reply(`Apk Name: ${title}\nDescription: ${desc}\nLicense: ${license}\n__[Download F-Droid](https://f-droid.org/F-Droid.apk)__`, {
+            parse_mode: 'markdown', reply_markup: key
+           })
+        }).catch(e => {
+         console.log(e);
+    })
+})
 
 bot.start();
